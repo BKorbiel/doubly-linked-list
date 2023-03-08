6d2ee683d58023a02e63e135ff67af9277dbb605
BKorbiel <109678632+BKorbiel@users.noreply.github.com>
2023-03-08 17:34:38 +0100

    Added head and improved list

--- main.cpp
+++ main.cpp
@@ -2,265 +2,351 @@
 
 using namespace std;
 
-struct MY_LIST {
-    long long int* data;
-    MY_LIST* prev;
-    MY_LIST* next;
+
+struct NODE {
+    long long int data;
+    NODE* prev;
+    NODE* next;
 };
 
+struct DBL { //head
+    NODE* first;
+    NODE* last;
+    int length;
+};
 
-MY_LIST* NewNode () {
-    MY_LIST* node = new MY_LIST;
-    node->data = new long long int;
+DBL* CreateList () {
+    DBL* list = new DBL;
+    list->first = NULL;
+    list->last = NULL;
+    list->length = 0;
+    return list;
+}
+
+NODE* NewNode () {
+    NODE* node = new NODE;
+    node->data = 0;
     node->prev = NULL;
     node->next = NULL;
     return node;
 }
 
-void DeleteNode ( MY_LIST* node ) {
-    delete (node->data);
+void DeleteNode ( NODE* node ) {
     delete node;
 }
 
-void Clear (MY_LIST** array){
-    MY_LIST* walkElem = *array;
-    MY_LIST* temp;
+void DeleteList (DBL** list){
+    if (*list==NULL) {
+        return;
+    }
+    NODE* walkElem = (*list)->first;
+
+    NODE* temp;
     while (walkElem!=NULL)
     {
         temp=walkElem->next;
         DeleteNode(walkElem);
         walkElem=temp;
     }
-    *array=NULL;
+    delete *list;
+    *list=NULL;
 }
 
-void ChangeValue ( MY_LIST** array, int index, long long int* new_node_data) {
-    int i=0;
-    MY_LIST* walkElem = *array;
-    while (i<index && walkElem!=NULL) {
+void ChangeValue ( DBL* list, int index, long long int* new_node_data) {
+    if (list==NULL || (list->length)<=index) {
+        return;
+    }
+    NODE* walkElem;
+    if (index>(list->length)/2) {
+        int i=(list->length)-1;
+        walkElem = list->last;
+        while (i>index) {
+            walkElem=walkElem->prev;
+            i--;
+        }
+    }
+    else {
+
+        int i=0;
+        walkElem = list->first;
+        while (i<index) {
+            walkElem=walkElem->next;
+            i++;
+        }
+    }
+    walkElem->data = *new_node_data;
+}
+
+void InsertPrev ( DBL* list, int index, long long int* new_node_data) {
+    if (list==NULL || (list->length)<=index) {
+        return;
+    }
+
+    NODE* new_node = NewNode();
+    new_node->data = *new_node_data;
+
+    if (index==0) {
+        new_node->prev=NULL;
+        new_node->next=list->first;
+        (list->first)->prev=new_node;
+        list->first=new_node;
+        (list->length)+=1;
+        return;
+    }
+
+    NODE* walkElem;
+    if (index>(list->length)/2) {
+        int i=(list->length)-1;
+        walkElem = list->last;
+        while (i>index) {
+            walkElem = walkElem->prev;
+            i--;
+        }
+    }
+    else {
+        int i=0;
+        walkElem = list->first;
+        while (i<index) {
+            walkElem = walkElem->next;
+            i++;
+        }
+    }
+
+    (list->length)+=1;
+    new_node->prev = walkElem->prev;
+    new_node->next = walkElem;
+    (walkElem->prev)->next = new_node;
+    walkElem->prev = new_node;
+}
+
+void InsertNext ( DBL* list, int index, long long int* new_node_data) {
+    NODE* new_node = NewNode();
+    new_node->data = *new_node_data;
+
+    if (list==NULL || (list->length)<=index) {
+        return;
+    }
+
+    NODE* walkElem;
+    if (index>(list->length)/2) {
+        int i=(list->length)-1;
+        walkElem = list->last;
+        while (i>index) {
+            walkElem = walkElem->prev;
+            i--;
+        }
+    }
+    else {
+        int i=0;
+        walkElem = list->first;
+        while (i<index) {
+            walkElem = walkElem->next;
+            i++;
+        }
+    }
+
+    (list->length)+=1;
+    new_node->prev = walkElem;
+    new_node->next = walkElem->next;
+    walkElem->next = new_node;
+    if (new_node->next!=NULL) {
+        (new_node->next)->prev = new_node;
+    }
+}
+
+void PushFront(DBL* list, long long int* new_node_data) {
+    if (list==NULL) {
+        return;
+    }
+    (list->length)+=1;
+    NODE* new_node = NewNode();
+    new_node->data = *new_node_data;
+
+    new_node->prev = NULL;
+    new_node->next = list->first;
+    if (list->first!=NULL) {
+        (list->first)->prev = new_node;
+    }
+    list->first=new_node;
+    if (list->length==1) {
+        list->last = new_node;
+    }
+
+}
+
+void PushBack(DBL* list, long long int* new_node_data) {
+    if (list==NULL) {
+        return;
+    }
+    (list->length)+=1;
+    NODE* new_node = NewNode();
+    new_node->data = *new_node_data;
+
+    new_node->next = NULL;
+    new_node->prev = list->last;
+    if (list->last!=NULL) {
+        (list->last)->next = new_node;
+    }
+    list->last=new_node;
+    if (list->length==1) {
+        list->first = new_node;
+    }
+}
+
+void Remove(DBL* list, int index) {
+    if (list==NULL || (list->length)<=index) {
+        return;
+    }
+
+    NODE* walkElem;
+    if (index>(list->length)/2) {
+        int i=(list->length)-1;
+        walkElem = list->last;
+        while (i>index) {
+            walkElem = walkElem->prev;
+            i--;
+        }
+    }
+    else {
+        int i=0;
+        walkElem = list->first;
+        while (i<index) {
+            walkElem = walkElem->next;
+            i++;
+        }
+    }
+
+    (list->length)-=1;
+    if (index==list->length) {
+        list->last=walkElem->prev;
+    } else {
+        (walkElem->next)->prev = walkElem->prev;
+    }
+    if (index==0) {
+        list->first=walkElem->next;
+    } else {
+        (walkElem->prev)->next = walkElem->next;
+    }
+
+    DeleteNode(walkElem);
+}
+
+void RemoveFront(DBL* list) {
+    Remove(list, 0);
+}
+
+void RemoveBack(DBL* list) {
+    if (list==NULL) {
+        return;
+    }
+
+    Remove(list, (list->length)-1);
+}
+
+void RemoveFirstByValue(DBL* list, long long int* value) {
+    if (list==NULL) {
+        return;
+    }
+
+    NODE* walkElem = list->first;
+
+    while (walkElem!=NULL && walkElem->data!=*value) {
         walkElem=walkElem->next;
-        i++;
     }
+
     if (walkElem!=NULL) {
-        *(walkElem->data) = *new_node_data;
-    }
-}
-
-void InsertPrev ( MY_LIST** array, int index, long long int* new_node_data) {
-    MY_LIST* new_node = NewNode();
-    *(new_node->data) = *new_node_data;
-
-    if (*array==NULL) {
-        *array=new_node;
-        new_node->next = NULL;
-        new_node->prev = NULL;
-    }
-    else if (index==0) {
-        MY_LIST* temp = *array;
-        *array = new_node;
-        new_node->next = temp;
-        new_node->prev = NULL;
-        temp->prev = new_node;
-    }
-    else {
-        MY_LIST* walkElem = *array;
-        int i=0;
-        while (i<index-1 && walkElem->next!=NULL) {
-            walkElem = walkElem->next;
-            i++;
-        }
-        if (walkElem->next!=NULL) {
-            (walkElem->next)->prev = new_node;
-            new_node->next = walkElem->next;
-            new_node->prev = walkElem;
-            walkElem->next = new_node;
+        (list->length)-=1;
+        if (walkElem->next==NULL) {
+            list->last=walkElem->prev;
         } else {
-            walkElem->next = new_node;
-            new_node->prev = walkElem;
-            new_node->next = NULL;
+            (walkElem->next)->prev = walkElem->prev;
         }
-    }
-}
-
-void InsertNext ( MY_LIST** array, int index, long long int* new_node_data) {
-    MY_LIST* new_node = NewNode();
-    *(new_node->data) = *new_node_data;
-
-    if (*array==NULL) {
-        *array=new_node;
-        new_node->next = NULL;
-        new_node->prev = NULL;
-    }
-    else {
-        MY_LIST* walkElem = *array;
-        int i=0;
-        while (i<index && walkElem->next!=NULL) {
-            walkElem = walkElem->next;
-            i++;
-        }
-        if (walkElem->next!=NULL) {
-            (walkElem->next)->prev = new_node;
-            new_node->next = walkElem->next;
-            new_node->prev = walkElem;
-            walkElem->next = new_node;
+        if (walkElem->prev==NULL) {
+            list->first=walkElem->next;
         } else {
-            walkElem->next = new_node;
-            new_node->prev = walkElem;
-            new_node->next = NULL;
+            (walkElem->prev)->next = walkElem->next;
         }
+        DeleteNode(walkElem);
     }
+
 }
 
-void PushFront(MY_LIST** array, long long int* new_node_data) {
-    InsertPrev(array, 0, new_node_data);
-}
+void RemoveAllByValue(DBL* list, long long int* value) {
+    if (list==NULL) {
+        return;
+    }
 
-void PushBack(MY_LIST** array, long long int* new_node_data) {
-    InsertNext(array, 2147483647, new_node_data); //work smart, not hard xD
-}
+    NODE* walkElem = list->first;
 
-void Remove(MY_LIST** array, int index) {
-    if (*array!=NULL) {
-        int i=0;
-        MY_LIST* walkElem = *array;
-        while (i<index && walkElem!=NULL) {
-            walkElem = walkElem->next;
-            i++;
-        }
-        if (walkElem!=NULL) {
-            if (walkElem->prev!=NULL) {
-                if (walkElem->next!=NULL) {
-                    (walkElem->prev)->next = walkElem->next;
-                    (walkElem->next)->prev = walkElem->prev;
-                } else {
-                    (walkElem->prev)->next=NULL;
-                }
+    while (walkElem!=NULL) {
+        NODE* temp = walkElem->next;
+        if (walkElem->data==*value) {
+            (list->length)-=1;
+            if (walkElem->next==NULL) {
+                list->last=walkElem->prev;
             } else {
-                if (walkElem->next!=NULL) {
-                    *array=walkElem->next;
-                    (walkElem->next)->prev = NULL;
-                } else {
-                    *array=NULL;
-                }
+                (walkElem->next)->prev = walkElem->prev;
+            }
+            if (walkElem->prev==NULL) {
+                list->first=walkElem->next;
+            } else {
+                (walkElem->prev)->next = walkElem->next;
             }
             DeleteNode(walkElem);
         }
+        walkElem = temp;
     }
 }
 
-void RemoveFront(MY_LIST** array) {
-    Remove(array, 0);
-}
-
-void RemoveBack(MY_LIST** array) {
-    if (*array!=NULL) {
-        MY_LIST* walkElem = *array;
-        while (walkElem->next!=NULL) {
-            walkElem=walkElem->next;
-        }
-        if (walkElem->prev!=NULL) {
-            (walkElem->prev)->next=NULL;
-        } else {
-            *array=NULL;
-        }
-        DeleteNode(walkElem);
-    }
-}
-
-void RemoveFirstByValue(MY_LIST** array, long long int value) {
-    MY_LIST* walkElem = *array;
-    while (walkElem!=NULL && *(walkElem->data)!=value) {
-        walkElem=walkElem->next;
-    }
-    if (walkElem!=NULL) {
-        if (walkElem->next!=NULL) {
-            if (walkElem->prev!=NULL) {
-                (walkElem->prev)->next = walkElem->next;
-                (walkElem->next)->prev = walkElem->prev;
-            }
-            else {
-                *array=walkElem->next;
-                (walkElem->next)->prev = NULL;
-            }
-        } else {
-            if (walkElem->prev!=NULL) {
-                (walkElem->prev)->next = NULL;
-            } else {
-                *array=NULL;
-            }
-        }
-        DeleteNode(walkElem);
-    }
-}
-
-void RemoveAllByValue(MY_LIST** array, long long int value) {
-    if (*array!=NULL) {
-        MY_LIST* walkElem = *array;
-        while (walkElem!=NULL) {
-            MY_LIST* temp = walkElem->next;
-            if (*(walkElem->data)!=value) {
-
-                if (walkElem->next!=NULL) {
-                    if (walkElem->prev!=NULL) {
-                        (walkElem->prev)->next = walkElem->next;
-                        (walkElem->next)->prev = walkElem->prev;
-                    }
-                    else {
-                        *array=walkElem->next;
-                        (walkElem->next)->prev = NULL;
-                    }
-                } else {
-                    if (walkElem->prev!=NULL) {
-                        (walkElem->prev)->next = NULL;
-                    } else {
-                        *array=NULL;
-                    }
-                }
-                DeleteNode(walkElem);
-            }
-            walkElem = temp;
-        }
-    }
-}
-
-long long int* Get(MY_LIST* array, int index) {
-    if (array==NULL) {
+long long int Get(DBL* list, int index) {
+    if (list==NULL || index>=(list->length)) {
         return NULL;
-    } else {
-        int i = 0;
-        MY_LIST* walkElem = array;
-        while (i<index && walkElem!=NULL) {
-            walkElem=walkElem->next;
+    }
+
+    NODE* walkElem;
+    if (index>(list->length)/2) {
+        int i=(list->length)-1;
+        walkElem = list->last;
+        while (i>index) {
+            walkElem = walkElem->prev;
+            i--;
+        }
+    }
+    else {
+        int i=0;
+        walkElem = list->first;
+        while (i<index) {
+            walkElem = walkElem->next;
             i++;
         }
-        if (walkElem!=NULL) {
-            return walkElem->data;
-        }
     }
+
+    return walkElem->data;
 }
 
-long long int* GetFirst(MY_LIST* array) {
-    return Get(array, 0);
-}
-
-long long int* GetLast(MY_LIST* array) {
-    if (array==NULL) {
+long long int GetFirst(DBL* list) {
+    if (list==NULL || list->length<1) {
         return NULL;
-    } else {
-        MY_LIST* walkElem = array;
-        while (walkElem->next!=NULL) {
-            walkElem = walkElem->next;
-        }
-        return walkElem->data;
     }
+    return (list->first)->data;
+}
+
+long long int GetLast(DBL* list) {
+    if (list==NULL || list->length<1) {
+        return NULL;
+    }
+    return (list->last)->data;
 }
 
 
-int Find(MY_LIST* array, long long int value) {
+int FindIndex(DBL* list, long long int value) {
+    if (list==NULL) {
+        return -1;
+    }
     int result=0;
-    MY_LIST* walkElem = array;
+    NODE* walkElem = list->first;
     while (walkElem!=NULL) {
-        if (*(walkElem->data)==value) {
+        if (walkElem->data==value) {
             return result;
         }
         walkElem = walkElem->next;
@@ -269,37 +355,61 @@ int Find(MY_LIST* array, long long int value) {
     return -1;
 }
 
-int GetLength(MY_LIST* array) {
-    int result = 0;
-    MY_LIST* walkElem = array;
-    while (walkElem!=NULL) {
-        walkElem = walkElem->next;
-        result++;
+int GetLength(DBL* list) {
+    if (list==NULL){
+        return NULL;
     }
-    return result;
+    return list->length;
 }
 
 int main() {
-    MY_LIST* listA = NewNode();
-    MY_LIST* listB = NewNode();
-    cout<<GetLength(listA)<<endl; //1
-    cout<<GetLength(listB)<<endl; //1
+    
+    /**************************EXAMPLES**********************************/
+    DBL* listA = CreateList();
+    DBL* listB = CreateList();
+
+    cout<<GetLength(listA)<<endl; //0
+    cout<<GetLength(listB)<<endl; //0
+
     long long int a=250, b=127;
-    ChangeValue(&listA, 0, &a); //listA = [&a = 250]
-    ChangeValue(&listB, 0, &b);  //listB = [&b = 127]
-    cout<<*GetFirst(listA)<<endl;  //250
-    cout<<*GetLast(listB)<<endl; //127
+    PushFront(listA, &b); //listA = [&b = 127]
+    PushFront(listB, &a); //listB = [&a = 250]
+
+    ChangeValue(listA, 0, &a); //listA = [&a = 250]
+    ChangeValue(listB, 0, &b);  //listB = [&b = 127]
+    cout<<GetFirst(listA)<<endl;  //250
+    cout<<GetLast(listB)<<endl; //127
+
     long long int d=15515251;
-    PushBack(&listA, &d);    //listA = [&a = 250, &d = 15515251]
-    PushFront(&listB, &d);  //listB = [&d = 15515251, &b = 127]
+    PushBack(listA, &d);    //listA = [&a = 250, &d = 15515251]
+    PushFront(listB, &d);  //listB = [&d = 15515251, &b = 127]
+
     long long int c=7777777777;
-    InsertNext(&listA, 1, &c);  //listA = [&a = 250, &d = 15515251, &c=7777777777]
-    InsertPrev(&listB, 1, &c);  //listB = [&d = 15515251, &c=7777777777, &b = 127]
-    cout<<GetLength(listA)<<" "<<Find(listA, 7777777777)<<endl;   //3 2
-    cout<<GetLength(listB)<<" "<<Find(listB, 7777777777)<<" "<<Find(listB, 747)<<endl;  //3 1 -1
-    Remove(&listA, 0); //listA = [&d = 15515251, &c=7777777777]
-    RemoveFirstByValue(&listB, 127); //listB = [&d = 15515251, &c=7777777777]
-    cout<<GetLength(listA)<<" "<<*Get(listA,0)<<endl;  //2 15515251
-    cout<<GetLength(listB)<<" "<<*GetFirst(listB)<<" "<<*GetLast(listB); //2 15515251 7777777777
+    InsertNext(listA, 1, &c);  //listA = [&a = 250, &d = 15515251, &c=7777777777]
+    InsertPrev(listB, 1, &c);  //listB = [&d = 15515251, &c=7777777777, &b = 127]
+
+    cout<<GetLength(listA)<<" "<<FindIndex(listA, 7777777777)<<endl;   //3 2
+    cout<<GetLength(listB)<<" "<<FindIndex(listB, 7777777777)<<" "<<FindIndex(listB, 747)<<endl;  //3 1 -1
+
+    Remove(listA, 0); //listA = [&d = 15515251, &c=7777777777]
+    RemoveFirstByValue(listB, &b); //listB = [&d = 15515251, &c=7777777777]
+
+    cout<<GetLength(listA)<<" "<<Get(listA,0)<<endl;  //2 15515251
+    cout<<GetLength(listB)<<" "<<GetFirst(listB)<<" "<<GetLast(listB)<<endl; //2 15515251 7777777777
+
+    PushBack(listA, &d);
+    InsertNext(listA, 1, &d); //listA = [&d = 15515251, &c=7777777777, &d = 15515251, &d = 15515251];
+    DeleteList(&listB); //listB=NULL
+
+    cout<<GetLength(listA)<<" "<<GetLength(listB)<<endl; //4, 0
+
+    RemoveAllByValue(listA, &d);
+    cout<<GetLength(listA)<<endl; //1
+
+    DeleteList(&listA);
+    cout<<GetLength(listA)<<endl; //0
+    
+    
     return 0;
 }
+
